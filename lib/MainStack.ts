7e19a79016fde4cce9710fcc974102dc4cfcce28
brfarn@amazon.com <brfarn@amazon.com>
2021-07-06 16:47:08 +1000

    Added stub deployment to force stack to wait on each deploy

--- lib/MainStack.ts
+++ lib/MainStack.ts
@@ -4,6 +4,7 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as waf from '@aws-cdk/aws-wafv2';
 import * as codedeploy from '@aws-cdk/aws-codedeploy';
 import * as synth from '@aws-cdk/aws-synthetics';
+import * as cw from '@aws-cdk/aws-cloudwatch';
 import * as democonstruct from "@demos/sharedcdkconstruct";
 import rules from "./ApiRules";
 import * as path from "path";
@@ -12,21 +13,22 @@ export class DemosCdkApiPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // LAMBDA FUNCTION
+
     const demoFunciton = new democonstruct.DemoFunction(this, "DemoFunction", {});
 
     // Uncomment this line to cause the security tests to fail
     //demoFunciton.LambdaFunction.addToRolePolicy(new iam.PolicyStatement({ actions: [ "iam:CreateUser" ], resources: [ "*" ] }))
-
-    const application = new codedeploy.LambdaApplication(this, 'CodeDeployApplication', {
-      applicationName: 'DemoLambdaApp', // optional property
-    });
 
     const versionAlias = new lambda.Alias(this, 'alias', {
       aliasName: 'prod',
       version: demoFunciton.LambdaFunction.currentVersion,
     });
 
-    // An API Gateway to make the Lambda web-accessible
+
+    // API GATEWAY
+
+
     const api = new apigw.LambdaRestApi(this, 'Gateway', {
       description: 'Endpoint for a simple Lambda-powered web service',
       handler: versionAlias
@@ -43,13 +45,16 @@ export class DemosCdkApiPipelineStack extends cdk.Stack {
       },
     });
 
+
+    // CANARY
+
     const association = new waf.CfnWebACLAssociation(this, "ApiWafAssociation", {
       resourceArn: `arn:${cdk.Aws.PARTITION}:apigateway:${cdk.Aws.REGION}::/restapis/${api.restApiId}/stages/prod`,
       webAclArn: acl.attrArn,
     });
     association.node.addDependency(api);
 
-    new synth.Canary(this, "WafSynethetics", {
+    const canary = new synth.Canary(this, "WafSynethetics", {
       runtime: synth.Runtime.SYNTHETICS_NODEJS_PUPPETEER_3_1,
       test: synth.Test.custom({
         code: synth.Code.fromAsset(path.resolve(__dirname, "../canaries/waf")),
@@ -62,10 +67,44 @@ export class DemosCdkApiPipelineStack extends cdk.Stack {
       }
     });
 
+
+    // CODE DEPLOY FOR API GATEWAY
+
+    const application = new codedeploy.LambdaApplication(this, 'CodeDeployApplication', {
+      applicationName: 'DemoLambdaApp', // optional property
+    });
+
     new codedeploy.LambdaDeploymentGroup(this, 'BlueGreenDeployment', {
       application,
       alias: versionAlias,
       deploymentConfig: codedeploy.LambdaDeploymentConfig.LINEAR_10PERCENT_EVERY_1MINUTE,
     });
+
+
+    // CODE DEPLOY STUB FOR STACK DEPLOYMENT
+    const stubLambda = new lambda.Function(this, "StackDeployStub", {
+      code: lambda.AssetCode.fromInline(`console.log("Stub last generated at ${Date.now().toString()}")`),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_12_X,
+    });
+
+    const stubAlias = new lambda.Alias(this, 'StubAlias', {
+      aliasName: 'prod',
+      version: stubLambda.currentVersion,
+    });
+
+    new codedeploy.LambdaDeploymentGroup(this, 'StackDeployment', {
+      application,
+      alias: stubAlias,
+      deploymentConfig: codedeploy.LambdaDeploymentConfig.LINEAR_10PERCENT_EVERY_1MINUTE,
+      alarms: [
+        new cw.Alarm(this, "SyntheticsFailureAlarm", {
+          evaluationPeriods: 1,
+          metric: canary.metricFailed(),
+          threshold: 1,
+        }),
+      ],
+    });
+
   }
 }
