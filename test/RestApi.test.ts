ac489c2dcd9d5a4f7ec632f8d9a633b9b252be39
Phone Myint Kyaw <phonemyintkyaw@hydramist.io>
2021-08-25 04:24:04 +0000

    Add tests and reduce the stack scope to RestApi only

--- test/RestApi.test.ts
+++ test/RestApi.test.ts
@@ -1,13 +1,19 @@
 import { Template } from '@aws-cdk/assertions';
+import { Construct } from "constructs";
 import * as cdk from 'aws-cdk-lib';
-import * as MainStack from '../lib/MainStack';
-import * as RestApi from '../lib/RestApi';
-import { Stack } from '@aws-cdk/core';
+import MainApi from '../lib/RestApi';
+
+class RestApiStack extends cdk.Stack {
+      constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+    
+        new MainApi(this, "MainAPI");
+      }};
 
 describe('REST API Test', () => {
 
     const app = new cdk.App();
-    const stack = new MainStack.DemosCdkApiPipelineStack(app, 'MyTestStack');
+    const stack = new RestApiStack(app, 'RestApiStack');
     const assert = Template.fromJSON(app.synth().getStackArtifact(stack.artifactId).template);
 
     test('REST API Endpoint == 1', () => {
@@ -28,10 +34,25 @@ describe('REST API Test', () => {
        }); 
     });
     
-    test('Validate Lambda function Runtime == nodejs14.x', () => {
+    test('Validate Lambda function Runtime', () => {
        assert.hasResourceProperties('AWS::Lambda::Function', {
           Runtime: 'nodejs14.x' 
        });
     });
-}); 
+    
+    test('CodeDeploy Alarm count >= 1', () => {
+        assert.findResources('AWS::CodeDeploy::DeploymentGroup').forEach( EachDeploymentGroup => {
+            expect(EachDeploymentGroup.Properties.AlarmConfiguration.Alarms.length).toBeGreaterThanOrEqual(1);
+        });
+        
+    })
+    
+    test('Check if auto rollback enabled', () => {
+        assert.hasResourceProperties('AWS::CodeDeploy::DeploymentGroup', {
+            AutoRollbackConfiguration: {
+                Enabled: true,
+            }
+        })
+    })
 
+}); 
