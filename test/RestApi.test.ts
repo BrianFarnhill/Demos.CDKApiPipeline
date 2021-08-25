09300e6b43c7fb654f4a2113913911b01d96c67c
Phone Myint Kyaw <phonemyintkyaw@hydramist.io>
2021-08-25 01:59:41 +0000

    Add Test: Check if CodeDeploy for Lambda exist

--- test/RestApi.test.ts
+++ test/RestApi.test.ts
@@ -1,6 +1,8 @@
 import { Template } from '@aws-cdk/assertions';
 import * as cdk from 'aws-cdk-lib';
 import * as MainStack from '../lib/MainStack';
+import * as RestApi from '../lib/RestApi';
+import { Stack } from '@aws-cdk/core';
 
 describe('REST API Test', () => {
 
@@ -12,5 +14,12 @@ describe('REST API Test', () => {
         expect(assert.findResources('AWS::ApiGateway::RestApi').length).toBe(1); // Find the number of REST API endpoints
     });
     
+    test('Check if CodeDeploy for Lambda exist', ()=> {
+        assert.hasResource('AWS::CodeDeploy::Application', {
+            Properties: {
+                ComputePlatform: 'Lambda'    
+            }
+        });
+    });
 }); 
 
