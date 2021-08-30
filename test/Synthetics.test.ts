96fb3e9b32a6f3da43c53ff1b50274c74712943f
brfarn@amazon.com <brfarn@amazon.com>
2021-08-30 15:20:24 +1000

    Added test for synthetics

---
+++ test/Synthetics.test.ts
@@ -0,0 +1,66 @@
+import { Match, Template } from '@aws-cdk/assertions';
+import { Stack, App, aws_apigateway as apigw }  from 'aws-cdk-lib';
+import Synthetics from '../lib/Synthetics';
+
+describe('Synthetics tests', () => {
+
+    const app = new App();
+    const stack = new Stack(app, 'MyTestStack');
+
+    const mockApi = new apigw.RestApi(stack, 'TestApi', {});
+    mockApi.root.addMethod('GET', new apigw.MockIntegration({}));
+
+    new Synthetics(stack, 'SyntheticsTest', {
+        ApiGateway: mockApi,
+    });
+
+    const assert = Template.fromJSON(app.synth().getStackArtifact(stack.artifactId).template);
+
+    test('Contains a single synthetics canary', () => {
+        assert.resourceCountIs('AWS::Synthetics::Canary', 1);
+    });
+
+    test('The canary runs with the Puppeteer 3.2 runtime', () => {
+        assert.hasResourceProperties('AWS::Synthetics::Canary', {
+            RuntimeVersion: 'syn-nodejs-puppeteer-3.2',
+        });
+    });
+
+    test('The canary runs once a minute', () => {
+        assert.hasResourceProperties('AWS::Synthetics::Canary', {
+            Schedule: {
+                Expression: 'rate(1 minute)',
+            },
+        });
+    });
+
+    test('Contains a single S3 bucket for storing canary results', () => {
+        assert.resourceCountIs('AWS::S3::Bucket', 1);
+    });
+
+    test('The S3 bucket deletes items that are 31 days old', () => {
+        assert.hasResourceProperties('AWS::S3::Bucket', {
+            LifecycleConfiguration: {
+                Rules: Match.arrayWith([
+                  {
+                    ExpirationInDays: 31,
+                    Status: "Enabled"
+                  }
+                ]),
+              }
+        });
+    });
+
+    test('Contains a a CloudWatch alarm that is tied to failing CloudWatch synthetics', () => {
+        assert.hasResourceProperties('AWS::CloudWatch::Alarm', {
+            Dimensions: Match.arrayWith([
+                {
+                  Name: 'CanaryName',
+                  Value: Match.objectLike({}),
+                }
+              ]),
+              MetricName: 'Failed requests',
+              Namespace: 'CloudWatchSynthetics',
+        });
+    });
+}); 
